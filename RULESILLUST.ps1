6642ee370e819d06bbb970a79d4bc25e2336a1f8
mskaufde <164560692+mskaufde@users.noreply.github.com>
2024-09-27 23:49:59 +0100

    Update RULESILLUST.ps1

--- RULESILLUST.ps1
+++ RULESILLUST.ps1
@@ -6,7 +6,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Exit
 }
 
-$folderPath = "C:\Program Files\Adobe\Adobe Photoshop 2023"
+$folderPath = "C:\Program Files\Adobe\Adobe Illustrator 2023\Support Files\Contents\Windows"
 
 # Create outbound rules for executables in the folder
 Get-ChildItem -Path $folderPath -Filter *.exe -Recurse | ForEach-Object {
