e2cb4eded55c703fba114ada68aa356822047125
mskaufde <164560692+mskaufde@users.noreply.github.com>
2024-09-29 15:50:40 +0100

    Update lightrmclsc.ps1

--- lightrmclsc.ps1
+++ lightrmclsc.ps1
@@ -1,1 +1,20 @@
-C:\Program Files\Adobe\Adobe Lightroom Classic
+# Check if the script is running with administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Relaunch the script as an administrator
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-File", $MyInvocation.MyCommand.Path
+    # Exit the current session
+    Exit
+}
+
+$folderPath = "C:\Program Files\Adobe\Adobe Lightroom Classic"
+
+# Create outbound rules for executables in the folder
+Get-ChildItem -Path $folderPath -Filter *.exe -Recurse | ForEach-Object {
+    New-NetFirewallRule -DisplayName "Block_Outbound_$($_.Name)" -Direction Outbound -Action Block -Program $_.FullName
+}
+
+# Create inbound rules for executables in the folder
+Get-ChildItem -Path $folderPath -Filter *.exe -Recurse | ForEach-Object {
+    New-NetFirewallRule -DisplayName "Block_Inbound_$($_.Name)" -Direction Inbound -Action Block -Program $_.FullName
+}
+Get-AppxPackage -AllUsers *AdobeNotificationClient* | Remove-AppxPackage
